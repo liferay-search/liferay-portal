5f1e8705642a06f1e3ad5f8d606257601118c86b
Bryan Engler <bryan.engler@liferay.com>
2020-08-31 12:09:33 -0700

    LPS-116892 add note for the API key in the examples

--- modules/dxp/apps/portal-search-tuning/portal-search-tuning-blueprints-bulk-loader-poc/src/main/java/com/liferay/portal/search/tuning/blueprints/bulkloader/poc/internal/portlet/action/ImportMVCActionCommand.java
+++ modules/dxp/apps/portal-search-tuning/portal-search-tuning-blueprints-bulk-loader-poc/src/main/java/com/liferay/portal/search/tuning/blueprints/bulkloader/poc/internal/portlet/action/ImportMVCActionCommand.java
@@ -318,23 +318,25 @@ public class ImportMVCActionCommand extends BaseMVCActionCommand {
 
 	/*
 	Location data was found using the Google "Places API". Additional data sets
-	can be added by copying the json results of a request into a file and
-	putting an entry in fileNameCityMap.
+	can be added by copying the results of a "Places API" request into a .json
+	file, placing the .json file in the resources directory, and putting an
+	entry in fileNameCityMap below.
 
-	An API Key is needed to perform requests
-	https://developers.google.com/places/web-service/get-api-key
+	An API Key is needed to perform requests, see
+	https://liferay.slack.com/archives/C0154CEGR3Q/p1598901770005800
 
-	Example requests:
+	Example "Places API" requests:
+
 	Restaurants within ~10 miles of Los Angeles
 	https://maps.googleapis.com/maps/api/place/nearbysearch/json?key=$API_KEY&location=34.061645,-118.261353&radius=15000&type=restaurant
 
 	Tourist Attractions within ~1 mile of New York
 	https://maps.googleapis.com/maps/api/place/nearbysearch/json?key=$API_KEY&location=40.761619,-73.972851&radius=1500&type=tourist_attraction
 
-	List of supported "types"
+	List of supported "types" for a "Places API" request:
 	https://developers.google.com/places/web-service/supported_types
 
-	json results formatted with https://jsonformatter.curiousconcept.com/
+	json results were formatted with https://jsonformatter.curiousconcept.com/
 	 */
 
 	private static final Map<String, String> fileNameCityMap =
