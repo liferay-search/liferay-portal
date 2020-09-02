c95cfc4d7aa94f0ae526fe8ff4b0c6c8089eddfd
Petteri Karttunen <petteri.karttunen@liferay.com>
2020-09-02 14:23:22 +0300

    Rebuild service against the new master.

--- modules/dxp/apps/portal-search-tuning/portal-search-tuning-blueprints-service/src/main/java/com/liferay/portal/search/tuning/blueprints/service/http/BlueprintServiceHttp.java
+++ modules/dxp/apps/portal-search-tuning/portal-search-tuning-blueprints-service/src/main/java/com/liferay/portal/search/tuning/blueprints/service/http/BlueprintServiceHttp.java
@@ -186,6 +186,47 @@ public class BlueprintServiceHttp {
 		}
 	}
 
+	public static com.liferay.portal.search.tuning.blueprints.model.Blueprint
+			getBlueprint(HttpPrincipal httpPrincipal, long blueprintId)
+		throws com.liferay.portal.kernel.exception.PortalException {
+
+		try {
+			MethodKey methodKey = new MethodKey(
+				BlueprintServiceUtil.class, "getBlueprint",
+				_getBlueprintParameterTypes3);
+
+			MethodHandler methodHandler = new MethodHandler(
+				methodKey, blueprintId);
+
+			Object returnObj = null;
+
+			try {
+				returnObj = TunnelUtil.invoke(httpPrincipal, methodHandler);
+			}
+			catch (Exception exception) {
+				if (exception instanceof
+						com.liferay.portal.kernel.exception.PortalException) {
+
+					throw (com.liferay.portal.kernel.exception.PortalException)
+						exception;
+				}
+
+				throw new com.liferay.portal.kernel.exception.SystemException(
+					exception);
+			}
+
+			return (com.liferay.portal.search.tuning.blueprints.model.Blueprint)
+				returnObj;
+		}
+		catch (com.liferay.portal.kernel.exception.SystemException
+					systemException) {
+
+			_log.error(systemException, systemException);
+
+			throw systemException;
+		}
+	}
+
 	public static java.util.List
 		<com.liferay.portal.search.tuning.blueprints.model.Blueprint>
 			getGroupBlueprints(
@@ -195,7 +236,7 @@ public class BlueprintServiceHttp {
 		try {
 			MethodKey methodKey = new MethodKey(
 				BlueprintServiceUtil.class, "getGroupBlueprints",
-				_getGroupBlueprintsParameterTypes3);
+				_getGroupBlueprintsParameterTypes4);
 
 			MethodHandler methodHandler = new MethodHandler(
 				methodKey, companyId, type, start, end);
@@ -232,7 +273,7 @@ public class BlueprintServiceHttp {
 		try {
 			MethodKey methodKey = new MethodKey(
 				BlueprintServiceUtil.class, "getGroupBlueprints",
-				_getGroupBlueprintsParameterTypes4);
+				_getGroupBlueprintsParameterTypes5);
 
 			MethodHandler methodHandler = new MethodHandler(
 				methodKey, companyId, status, type, start, end);
@@ -272,7 +313,7 @@ public class BlueprintServiceHttp {
 		try {
 			MethodKey methodKey = new MethodKey(
 				BlueprintServiceUtil.class, "getGroupBlueprints",
-				_getGroupBlueprintsParameterTypes5);
+				_getGroupBlueprintsParameterTypes6);
 
 			MethodHandler methodHandler = new MethodHandler(
 				methodKey, companyId, status, type, start, end,
@@ -313,7 +354,7 @@ public class BlueprintServiceHttp {
 		try {
 			MethodKey methodKey = new MethodKey(
 				BlueprintServiceUtil.class, "getGroupBlueprints",
-				_getGroupBlueprintsParameterTypes6);
+				_getGroupBlueprintsParameterTypes7);
 
 			MethodHandler methodHandler = new MethodHandler(
 				methodKey, companyId, type, start, end, orderByComparator);
@@ -347,7 +388,7 @@ public class BlueprintServiceHttp {
 		try {
 			MethodKey methodKey = new MethodKey(
 				BlueprintServiceUtil.class, "getGroupBlueprintsCount",
-				_getGroupBlueprintsCountParameterTypes7);
+				_getGroupBlueprintsCountParameterTypes8);
 
 			MethodHandler methodHandler = new MethodHandler(
 				methodKey, companyId, type);
@@ -379,7 +420,7 @@ public class BlueprintServiceHttp {
 		try {
 			MethodKey methodKey = new MethodKey(
 				BlueprintServiceUtil.class, "getGroupBlueprintsCount",
-				_getGroupBlueprintsCountParameterTypes8);
+				_getGroupBlueprintsCountParameterTypes9);
 
 			MethodHandler methodHandler = new MethodHandler(
 				methodKey, companyId, status, type);
@@ -395,47 +436,6 @@ public class BlueprintServiceHttp {
 			}
 
 			return ((Integer)returnObj).intValue();
-		}
-		catch (com.liferay.portal.kernel.exception.SystemException
-					systemException) {
-
-			_log.error(systemException, systemException);
-
-			throw systemException;
-		}
-	}
-
-	public static com.liferay.portal.search.tuning.blueprints.model.Blueprint
-			getBlueprint(HttpPrincipal httpPrincipal, long blueprintId)
-		throws com.liferay.portal.kernel.exception.PortalException {
-
-		try {
-			MethodKey methodKey = new MethodKey(
-				BlueprintServiceUtil.class, "getBlueprint",
-				_getBlueprintParameterTypes9);
-
-			MethodHandler methodHandler = new MethodHandler(
-				methodKey, blueprintId);
-
-			Object returnObj = null;
-
-			try {
-				returnObj = TunnelUtil.invoke(httpPrincipal, methodHandler);
-			}
-			catch (Exception exception) {
-				if (exception instanceof
-						com.liferay.portal.kernel.exception.PortalException) {
-
-					throw (com.liferay.portal.kernel.exception.PortalException)
-						exception;
-				}
-
-				throw new com.liferay.portal.kernel.exception.SystemException(
-					exception);
-			}
-
-			return (com.liferay.portal.search.tuning.blueprints.model.Blueprint)
-				returnObj;
 		}
 		catch (com.liferay.portal.kernel.exception.SystemException
 					systemException) {
@@ -507,27 +507,27 @@ public class BlueprintServiceHttp {
 		};
 	private static final Class<?>[] _deleteBlueprintParameterTypes2 =
 		new Class[] {long.class};
-	private static final Class<?>[] _getGroupBlueprintsParameterTypes3 =
-		new Class[] {long.class, int.class, int.class, int.class};
+	private static final Class<?>[] _getBlueprintParameterTypes3 = new Class[] {
+		long.class
+	};
 	private static final Class<?>[] _getGroupBlueprintsParameterTypes4 =
-		new Class[] {long.class, int.class, int.class, int.class, int.class};
+		new Class[] {long.class, int.class, int.class, int.class};
 	private static final Class<?>[] _getGroupBlueprintsParameterTypes5 =
+		new Class[] {long.class, int.class, int.class, int.class, int.class};
+	private static final Class<?>[] _getGroupBlueprintsParameterTypes6 =
 		new Class[] {
 			long.class, int.class, int.class, int.class, int.class,
 			com.liferay.portal.kernel.util.OrderByComparator.class
 		};
-	private static final Class<?>[] _getGroupBlueprintsParameterTypes6 =
+	private static final Class<?>[] _getGroupBlueprintsParameterTypes7 =
 		new Class[] {
 			long.class, int.class, int.class, int.class,
 			com.liferay.portal.kernel.util.OrderByComparator.class
 		};
-	private static final Class<?>[] _getGroupBlueprintsCountParameterTypes7 =
-		new Class[] {long.class, int.class};
 	private static final Class<?>[] _getGroupBlueprintsCountParameterTypes8 =
+		new Class[] {long.class, int.class};
+	private static final Class<?>[] _getGroupBlueprintsCountParameterTypes9 =
 		new Class[] {long.class, int.class, int.class};
-	private static final Class<?>[] _getBlueprintParameterTypes9 = new Class[] {
-		long.class
-	};
 	private static final Class<?>[] _updateBlueprintParameterTypes10 =
 		new Class[] {
 			long.class, java.util.Map.class, java.util.Map.class, String.class,
