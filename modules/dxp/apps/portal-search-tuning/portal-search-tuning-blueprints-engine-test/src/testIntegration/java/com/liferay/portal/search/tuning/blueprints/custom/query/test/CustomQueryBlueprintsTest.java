896520aa4ff66e4cac96d6994807821d0ce0e75d
André de Oliveira <andre.oliveira@liferay.com>
2021-03-23 21:00:17 -0700

    LPS-124988 portal-search-tuning-blueprints-engine-test: [Test] Integration Test: "Boost-A-Clause"
    https://issues.liferay.com/browse/LPS-124988

--- modules/dxp/apps/portal-search-tuning/portal-search-tuning-blueprints-engine-test/src/testIntegration/java/com/liferay/portal/search/tuning/blueprints/custom/query/test/CustomQueryBlueprintsTest.java
+++ modules/dxp/apps/portal-search-tuning/portal-search-tuning-blueprints-engine-test/src/testIntegration/java/com/liferay/portal/search/tuning/blueprints/custom/query/test/CustomQueryBlueprintsTest.java
@@ -16,27 +16,22 @@ package com.liferay.portal.search.tuning.blueprints.custom.query.test;
 
 import com.liferay.arquillian.extension.junit.bridge.junit.Arquillian;
 import com.liferay.journal.test.util.JournalTestUtil;
-import com.liferay.layout.test.util.LayoutTestUtil;
-import com.liferay.petra.string.CharPool;
+import com.liferay.petra.string.StringPool;
 import com.liferay.portal.kernel.language.LanguageUtil;
 import com.liferay.portal.kernel.model.Group;
-import com.liferay.portal.kernel.model.Layout;
 import com.liferay.portal.kernel.model.User;
 import com.liferay.portal.kernel.portlet.bridges.mvc.MVCActionCommand;
-import com.liferay.portal.kernel.security.permission.PermissionCheckerFactory;
-import com.liferay.portal.kernel.security.permission.PermissionThreadLocal;
 import com.liferay.portal.kernel.service.CompanyLocalService;
 import com.liferay.portal.kernel.service.ServiceContext;
-import com.liferay.portal.kernel.test.ReflectionTestUtil;
 import com.liferay.portal.kernel.test.portlet.MockLiferayPortletActionRequest;
 import com.liferay.portal.kernel.test.portlet.MockLiferayPortletActionResponse;
 import com.liferay.portal.kernel.test.rule.AggregateTestRule;
 import com.liferay.portal.kernel.test.rule.DeleteAfterTestRun;
+import com.liferay.portal.kernel.test.util.RandomTestUtil;
 import com.liferay.portal.kernel.test.util.ServiceContextTestUtil;
 import com.liferay.portal.kernel.test.util.TestPropsValues;
 import com.liferay.portal.kernel.theme.ThemeDisplay;
 import com.liferay.portal.kernel.util.Constants;
-import com.liferay.portal.kernel.util.ListUtil;
 import com.liferay.portal.kernel.util.LocaleUtil;
 import com.liferay.portal.kernel.util.StringBundler;
 import com.liferay.portal.kernel.util.StringUtil;
@@ -46,6 +41,7 @@ import com.liferay.portal.search.searcher.SearchRequestBuilderFactory;
 import com.liferay.portal.search.searcher.SearchResponse;
 import com.liferay.portal.search.searcher.Searcher;
 import com.liferay.portal.search.test.util.DocumentsAssert;
+import com.liferay.portal.search.test.util.SearchTestRule;
 import com.liferay.portal.search.tuning.blueprints.admin.web.internal.constants.BlueprintsAdminMVCCommandNames;
 import com.liferay.portal.search.tuning.blueprints.admin.web.internal.constants.BlueprintsAdminWebKeys;
 import com.liferay.portal.search.tuning.blueprints.engine.constants.SearchContextAttributeKeys;
@@ -57,14 +53,9 @@ import com.liferay.portal.test.rule.PermissionCheckerMethodTestRule;
 import com.liferay.portletmvc4spring.test.mock.web.portlet.MockActionRequest;
 import com.liferay.users.admin.test.util.search.GroupBlueprint;
 import com.liferay.users.admin.test.util.search.GroupSearchFixture;
-import com.liferay.users.admin.test.util.search.UserSearchFixture;
 
-import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
-
-import javax.portlet.ActionRequest;
-import javax.portlet.ActionResponse;
 
 import org.junit.Before;
 import org.junit.ClassRule;
@@ -88,87 +79,73 @@ public class CustomQueryBlueprintsTest {
 
 	@Before
 	public void setUp() throws Exception {
-		_userSearchFixture = new UserSearchFixture();
+		GroupSearchFixture groupSearchFixture = new GroupSearchFixture();
 
-		_userSearchFixture.setUp();
+		_group = groupSearchFixture.addGroup(new GroupBlueprint());
 
-		_users = _userSearchFixture.getUsers();
+		_groups = groupSearchFixture.getGroups();
 
-		_addGroupAndUser();
-
-		_layout = LayoutTestUtil.addLayout(_group.getGroupId());
-
-		_createJournalArticles();
+		_user = TestPropsValues.getUser();
 	}
 
 	@Test
 	public void testBoostAClause() throws Exception {
-		_addCompanyBlueprint(readConfiguration());
+		addArticle("cafe 1", RandomTestUtil.randomString());
+		addArticle("cafe 2", RandomTestUtil.randomString());
+		addArticle("cafe 3", "los angeles");
 
-		List<String> expectedValues = new ArrayList<>();
+		addBlueprint();
 
-		expectedValues.add(_titles[2]);
-		expectedValues.add(_titles[0]);
-		expectedValues.add(_titles[1]);
-
-		_assertSearch(
-			"localized_title", "restaurant 1", String.valueOf(expectedValues));
+		_assertSearch("cafe 2", "[cafe 3, cafe 2, cafe 1]");
 	}
 
 	@Test
 	public void testMustBeClause() throws Exception {
-		_addCompanyBlueprint(readConfiguration());
+		addArticle("cafe 1", RandomTestUtil.randomString());
+		addArticle("cafe 2", RandomTestUtil.randomString());
+		addArticle("cafe 3", "los angeles");
 
-		List<String> expectedValues = ListUtil.fromArray(_titles[2]);
+		addBlueprint();
 
-		_assertSearch(
-			"localized_title", "restaurant 1", String.valueOf(expectedValues));
+		_assertSearch("cafe 2", "[cafe 3]");
 	}
 
 	@Test
 	public void testMustNotBeClause() throws Exception {
-		_addCompanyBlueprint(readConfiguration());
+		addArticle("cafe 1", RandomTestUtil.randomString());
+		addArticle("cafe 2", RandomTestUtil.randomString());
+		addArticle("cafe 3", "los angeles");
 
-		List<String> expectedValues = new ArrayList<>();
+		addBlueprint();
 
-		expectedValues.add(_titles[0]);
-		expectedValues.add(_titles[1]);
-
-		_assertSearch(
-			"localized_title", "restaurant 1", String.valueOf(expectedValues));
+		_assertSearch("cafe 2", "[cafe 2, cafe 1]");
 	}
 
 	@Test
 	public void testTextMatch() throws Exception {
-		_addCompanyBlueprint(readConfiguration());
+		addArticle("restaurant 1", RandomTestUtil.randomString());
+		addArticle("restaurant 2", RandomTestUtil.randomString());
+		addArticle("cafe 1", RandomTestUtil.randomString());
 
-		List<String> expectedValues = ListUtil.fromArray(_titles[2]);
+		addBlueprint();
 
-		_assertSearch(
-			"localized_title", "cafe", String.valueOf(expectedValues));
+		_assertSearch("cafe", "[cafe 1]");
 	}
+
+	@Rule
+	public SearchTestRule searchTestRule = new SearchTestRule();
 
 	@Rule
 	public TestName testName = new TestName();
 
-	protected String readConfiguration() {
-		Class<?> clazz = getClass();
-
-		StringBundler sb = new StringBundler(5);
-
-		sb.append(clazz.getSimpleName());
-		sb.append(CharPool.PERIOD);
-		sb.append(testName.getMethodName());
-		sb.append(CharPool.PERIOD);
-		sb.append("json");
-
-		return StringUtil.read(clazz, sb.toString());
+	protected void addArticle(String title, String content) throws Exception {
+		JournalTestUtil.addArticle(_group.getGroupId(), title, content);
 	}
 
-	private void _addCompanyBlueprint(String configurationString)
-		throws Exception {
+	protected void addBlueprint() throws Exception {
+		String configurationString = readConfiguration();
 
-		Blueprint blueprint = _blueprintService.addCompanyBlueprint(
+		Blueprint blueprint = blueprintService.addCompanyBlueprint(
 			Collections.singletonMap(LocaleUtil.US, "Los Angeles Boost"),
 			Collections.singletonMap(LocaleUtil.US, "test Description"),
 			configurationString, "", 1, _getServiceContext());
@@ -178,82 +155,66 @@ public class CustomQueryBlueprintsTest {
 		_updateBlueprintWithMVCActionCommand(blueprint, configurationString);
 	}
 
-	private void _addGroupAndUser() throws Exception {
-		GroupSearchFixture groupSearchFixture = new GroupSearchFixture();
+	protected String readConfiguration() {
+		Class<?> clazz = getClass();
 
-		_group = groupSearchFixture.addGroup(new GroupBlueprint());
-
-		_groups = groupSearchFixture.getGroups();
-
-		_user = TestPropsValues.getUser();
-
-		PermissionThreadLocal.setPermissionChecker(
-			_permissionCheckerFactory.create(_user));
+		return StringUtil.read(
+			clazz,
+			StringBundler.concat(
+				clazz.getSimpleName(), StringPool.PERIOD,
+				testName.getMethodName(), ".json"));
 	}
 
-	private void _assertSearch(
-		String fieldName, String keywords, String expected) {
+	@Inject
+	protected BlueprintService blueprintService;
 
-		SearchResponse searchResponse = _searcher.search(
-			_searchRequestBuilderFactory.builder(
-			).addComplexQueryPart(
-				_complexQueryPartBuilderFactory.builder(
-				).field(
-					"screenName"
-				).occur(
-					"must_not"
-				).type(
-					"term"
-				).value(
-					"test"
-				).build()
+	@Inject
+	protected CompanyLocalService companyLocalService;
+
+	@Inject
+	protected ComplexQueryPartBuilderFactory complexQueryPartBuilderFactory;
+
+	@Inject(
+		filter = "mvc.command.name=" + BlueprintsAdminMVCCommandNames.EDIT_BLUEPRINT
+	)
+	protected MVCActionCommand mvcActionCommand;
+
+	@Inject
+	protected Searcher searcher;
+
+	@Inject
+	protected SearchRequestBuilderFactory searchRequestBuilderFactory;
+
+	private void _assertSearch(String keywords, String expected) {
+		SearchResponse searchResponse = searcher.search(
+			searchRequestBuilderFactory.builder(
 			).companyId(
 				_group.getCompanyId()
 			).groupIds(
 				_group.getGroupId()
+			).queryString(
+				keywords
 			).withSearchContext(
-				searchContext -> {
-					searchContext.setAttribute(
-						SearchContextAttributeKeys.BLUEPRINT_ID,
-						_blueprint.getBlueprintId());
-					searchContext.setKeywords(keywords);
-					searchContext.setLayout(_layout);
-					searchContext.setTimeZone(_user.getTimeZone());
-					searchContext.setUserId(_user.getUserId());
-				}
+				searchContext -> searchContext.setAttribute(
+					SearchContextAttributeKeys.BLUEPRINT_ID,
+					_blueprint.getBlueprintId())
 			).build());
 
 		DocumentsAssert.assertValues(
 			searchResponse.getRequestString(),
-			searchResponse.getDocumentsStream(), fieldName, expected);
+			searchResponse.getDocumentsStream(), "localized_title", expected);
 	}
 
-	private void _createJournalArticles() throws Exception {
-		String[] titles = {"restaurant 1", "restaurant 2", "la cafe"};
-
-		_titles = titles;
-
-		JournalTestUtil.addArticle(
-			_group.getGroupId(), _titles[0], "restaurant content");
-
-		JournalTestUtil.addArticle(
-			_group.getGroupId(), _titles[1], "restaurant content");
-
-		JournalTestUtil.addArticle(
-			_group.getGroupId(), _titles[2], "los angeles restaurant");
-	}
-
-	private MockActionRequest _getMockActionrequest() throws Exception {
+	private MockActionRequest _getMockActionRequest() throws Exception {
 		MockLiferayPortletActionRequest mockActionRequest =
 			new MockLiferayPortletActionRequest();
 
 		ThemeDisplay themeDisplay = new ThemeDisplay();
 
 		themeDisplay.setCompany(
-			_companyLocalService.getCompany(TestPropsValues.getCompanyId()));
+			companyLocalService.getCompany(TestPropsValues.getCompanyId()));
 		themeDisplay.setLanguageId(
 			LanguageUtil.getLanguageId(LocaleUtil.getDefault()));
-		themeDisplay.setPlid(_layout.getPlid());
 		themeDisplay.setScopeGroupId(_group.getGroupId());
 		themeDisplay.setUser(_user);
 
@@ -271,7 +232,7 @@ public class CustomQueryBlueprintsTest {
 			Blueprint blueprint, String configurationString)
 		throws Exception {
 
-		MockActionRequest mockActionRequest = _getMockActionrequest();
+		MockActionRequest mockActionRequest = _getMockActionRequest();
 
 		mockActionRequest.setParameter(
 			BlueprintsAdminWebKeys.BLUEPRINT_ID,
@@ -290,51 +251,17 @@ public class CustomQueryBlueprintsTest {
 		MockLiferayPortletActionResponse mockActionResponse =
 			new MockLiferayPortletActionResponse();
 
-		ReflectionTestUtil.invoke(
-			_mvcActionCommand, "doProcessAction",
-			new Class<?>[] {ActionRequest.class, ActionResponse.class},
-			mockActionRequest, mockActionResponse);
+		mvcActionCommand.processAction(mockActionRequest, mockActionResponse);
 	}
 
 	@DeleteAfterTestRun
 	private Blueprint _blueprint;
-
-	@Inject
-	private BlueprintService _blueprintService;
-
-	@Inject
-	private CompanyLocalService _companyLocalService;
-
-	@Inject
-	private ComplexQueryPartBuilderFactory _complexQueryPartBuilderFactory;
 
 	private Group _group;
 
 	@DeleteAfterTestRun
 	private List<Group> _groups;
 
-	private Layout _layout;
-
-	@Inject(
-		filter = "mvc.command.name=" + BlueprintsAdminMVCCommandNames.EDIT_BLUEPRINT
-	)
-	private MVCActionCommand _mvcActionCommand;
-
-	@Inject
-	private PermissionCheckerFactory _permissionCheckerFactory;
-
-	@Inject
-	private Searcher _searcher;
-
-	@Inject
-	private SearchRequestBuilderFactory _searchRequestBuilderFactory;
-
-	private String[] _titles;
 	private User _user;
-
-	@DeleteAfterTestRun
-	private List<User> _users;
-
-	private UserSearchFixture _userSearchFixture;
 
 }
