1c238903a28858410b880ec7d17c2f0b42b705de
André de Oliveira <andre.oliveira@liferay.com>
2021-09-16 11:21:54 -0500

    LPS-124256 WIP search-experiences-blueprints-service: Add a BlueprintLookupImpl of original Blueprint until all new SXPBlueprint database methods are available to call from tests

---
+++ modules/dxp/apps/search-experiences/search-experiences-blueprints-service/src/main/java/com/liferay/search/experiences/blueprints/internal/lookup/BlueprintLookupImpl.java
@@ -0,0 +1,67 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * The contents of this file are subject to the terms of the Liferay Enterprise
+ * Subscription License ("License"). You may not use this file except in
+ * compliance with the License. You can obtain a copy of the License by
+ * contacting Liferay, Inc. See the License for the specific language governing
+ * permissions and limitations under the License, including but not limited to
+ * distribution rights of the Software.
+ *
+ *
+ *
+ */
+
+package com.liferay.search.experiences.blueprints.internal.lookup;
+
+import com.liferay.portal.kernel.exception.PortalException;
+import com.liferay.portal.kernel.log.Log;
+import com.liferay.portal.kernel.log.LogFactoryUtil;
+import com.liferay.search.experiences.blueprints.Blueprint;
+import com.liferay.search.experiences.blueprints.BlueprintLookup;
+import com.liferay.search.experiences.blueprints.service.BlueprintLocalService;
+import com.liferay.search.experiences.exception.NoSuchSXPBlueprintException;
+
+import java.util.Optional;
+
+import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Reference;
+
+/**
+ * @author André de Oliveira
+ */
+@Component(service = BlueprintLookup.class)
+public class BlueprintLookupImpl implements BlueprintLookup {
+
+	@Override
+	public Optional<Blueprint> getBlueprintOptional(long blueprintId) {
+		return Optional.ofNullable(
+			_getBlueprint(blueprintId)
+		).map(
+			BlueprintImpl::new
+		);
+	}
+
+	private com.liferay.search.experiences.blueprints.model.Blueprint
+		_getBlueprint(long blueprintId) {
+
+		try {
+			return _blueprintLocalService.getBlueprint(blueprintId);
+		}
+		catch (NoSuchSXPBlueprintException noSuchSXPBlueprintException) {
+			_log.error(noSuchSXPBlueprintException);
+
+			return null;
+		}
+		catch (PortalException portalException) {
+			throw new RuntimeException(portalException);
+		}
+	}
+
+	private static final Log _log = LogFactoryUtil.getLog(
+		BlueprintLookupImpl.class);
+
+	@Reference
+	private BlueprintLocalService _blueprintLocalService;
+
+}
