4ecf2b6d7474ab1a687c661da8ba8df67c806d43
Kevin Tan <kevin.tan@liferay.com>
2022-06-14 22:54:10 -0700

    LPS-155753 portal-search-web: Only add scope to URL if scope selector is displayed
    Scope selector is only shown when the scope configuration is "Let the user choose"

--- modules/apps/portal-search/portal-search-web/src/main/resources/META-INF/resources/js/components/SearchBar.js
+++ modules/apps/portal-search/portal-search-web/src/main/resources/META-INF/resources/js/components/SearchBar.js
@@ -252,7 +252,7 @@ export default function SearchBar({
 			searchParams.delete(paginationStartParameterName);
 		}
 
-		if (scope) {
+		if (letUserChooseScope) {
 			searchParams.set(scopeParameterName, scope);
 		}
 
