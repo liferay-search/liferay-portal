c4fc9bbfe906c4419b27f2c8698232198b2a6487
Petteri Karttunen <petteri.karttunen@liferay.com>
2022-09-21 11:40:25 +0300

    LPS-161171 search-experiences-rest: gw buildRest

--- modules/dxp/apps/search-experiences/search-experiences-rest-impl/src/main/java/com/liferay/search/experiences/rest/internal/resource/v1_0/OpenAPIResourceImpl.java
+++ modules/dxp/apps/search-experiences/search-experiences-rest-impl/src/main/java/com/liferay/search/experiences/rest/internal/resource/v1_0/OpenAPIResourceImpl.java
@@ -99,6 +99,8 @@ public class OpenAPIResourceImpl {
 
 			add(KeywordQueryContributorResourceImpl.class);
 
+			add(MLModelResourceImpl.class);
+
 			add(ModelPrefilterContributorResourceImpl.class);
 
 			add(QueryPrefilterContributorResourceImpl.class);
