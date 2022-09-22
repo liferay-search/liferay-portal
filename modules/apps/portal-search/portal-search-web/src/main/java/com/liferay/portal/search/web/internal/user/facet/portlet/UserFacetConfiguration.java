4182ab0a0bc3b321caffdaed02b558706b73d81b
Almir Ferreira <almir.ferreira@liferay.com>
2022-09-22 18:35:49 -0300

    LPS-162050 LPS-163237 portal-search-web: Add getOrder and setOrder methods in UserFacetConfiguration
    https://issues.liferay.com/browse/LPS-163237

--- modules/apps/portal-search/portal-search-web/src/main/java/com/liferay/portal/search/web/internal/user/facet/portlet/UserFacetConfiguration.java
+++ modules/apps/portal-search/portal-search-web/src/main/java/com/liferay/portal/search/web/internal/user/facet/portlet/UserFacetConfiguration.java
@@ -23,8 +23,12 @@ public interface UserFacetConfiguration {
 
 	public int getMaxTerms();
 
+	public String getOrder();
+
 	public void setFrequencyThreshold(int frequencyThreshold);
 
 	public void setMaxTerms(int maxTerms);
+
+	public void setOrder(String order);
 
 }
