0db4bd9c8f5e975f6a670df70ee416643928bec4
Olivia Yu <olivia.yu@liferay.com>
2022-11-17 09:34:47 -0800

    LPS-168167 LPS-168293 search-experiences-web: Rename all variables of 'sentenceTransformProvider'

--- modules/dxp/apps/search-experiences/search-experiences-web/src/main/java/com/liferay/search/experiences/web/internal/configuration/admin/display/SemanticSearchConfigurationFormRenderer.java
+++ modules/dxp/apps/search-experiences/search-experiences-web/src/main/java/com/liferay/search/experiences/web/internal/configuration/admin/display/SemanticSearchConfigurationFormRenderer.java
@@ -99,12 +99,12 @@ public class SemanticSearchConfigurationFormRenderer
 			"modelTimeout",
 			ParamUtil.getInteger(httpServletRequest, "modelTimeout")
 		).put(
+			"sentenceTransformer",
+			ParamUtil.getString(httpServletRequest, "sentenceTransformer")
+		).put(
 			"sentenceTransformerEnabled",
 			ParamUtil.getBoolean(
 				httpServletRequest, "sentenceTransformerEnabled")
-		).put(
-			"sentenceTransformProvider",
-			ParamUtil.getString(httpServletRequest, "sentenceTransformProvider")
 		).put(
 			"textTruncationStrategy",
 			ParamUtil.getString(httpServletRequest, "textTruncationStrategy")
@@ -148,8 +148,8 @@ public class SemanticSearchConfigurationFormRenderer
 			setAvailableLanguageDisplayNames(
 				_getAvailableLanguageDisplayNames(httpServletRequest));
 		semanticSearchCompanyConfigurationDisplayContext.
-			setAvailableSentenceTranformProviders(
-				_getAvailableSentenceTranformProviders(httpServletRequest));
+			setAvailableSentenceTransformers(
+				_getAvailableSentenceTransformers(httpServletRequest));
 		semanticSearchCompanyConfigurationDisplayContext.
 			setAvailableTextTruncationStrategies(
 				_getAvailableTextTruncationStrategies(httpServletRequest));
@@ -172,9 +172,8 @@ public class SemanticSearchConfigurationFormRenderer
 		semanticSearchCompanyConfigurationDisplayContext.
 			setSentenceTransformerEnabled(
 				_semanticSearchConfiguration.sentenceTransformerEnabled());
-		semanticSearchCompanyConfigurationDisplayContext.
-			setSentenceTransformProvider(
-				_semanticSearchConfiguration.sentenceTransformProvider());
+		semanticSearchCompanyConfigurationDisplayContext.setSentenceTransformer(
+			_semanticSearchConfiguration.sentenceTransformer());
 		semanticSearchCompanyConfigurationDisplayContext.
 			setTextTruncationStrategy(
 				_semanticSearchConfiguration.textTruncationStrategy());
@@ -260,7 +259,7 @@ public class SemanticSearchConfigurationFormRenderer
 		return _sortByValue(availableLanguageDisplayNames);
 	}
 
-	private Map<String, String> _getAvailableSentenceTranformProviders(
+	private Map<String, String> _getAvailableSentenceTransformers(
 		HttpServletRequest httpServletRequest) {
 
 		return LinkedHashMapBuilder.put(
