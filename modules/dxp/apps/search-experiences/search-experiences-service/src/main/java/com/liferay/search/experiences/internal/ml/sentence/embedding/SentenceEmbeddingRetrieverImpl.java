21aca4d61298a9a88a95db4897d35b08f49c04c9
Olivia Yu <olivia.yu@liferay.com>
2022-11-17 09:37:06 -0800

    LPS-168167 LPS-168293 search-experiences-**: Keep consistent naming for semantic search configuration

--- modules/dxp/apps/search-experiences/search-experiences-service/src/main/java/com/liferay/search/experiences/internal/ml/sentence/embedding/SentenceEmbeddingRetrieverImpl.java
+++ modules/dxp/apps/search-experiences/search-experiences-service/src/main/java/com/liferay/search/experiences/internal/ml/sentence/embedding/SentenceEmbeddingRetrieverImpl.java
@@ -52,7 +52,7 @@ public class SentenceEmbeddingRetrieverImpl
 
 		SentenceTransformer sentenceTransformer =
 			_sentenceTransformerServiceTrackerMap.getService(
-				semanticSearchConfiguration.sentenceTransformProvider());
+				semanticSearchConfiguration.sentenceTransformer());
 
 		if (sentenceTransformer == null) {
 			return new Double[0];
