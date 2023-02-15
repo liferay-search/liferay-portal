e0e4af8bd9dd72640e500d8b848ac2c62b15f55f
Tibor Lipusz <tibor.lipusz@liferay.com>
2023-02-15 10:18:41 +0100

    LPS-174975 portal-workflow-metrics: Test - Remove document type from requests as it's no longer supported in 8.x

--- modules/dxp/apps/portal-workflow/portal-workflow-metrics-rest-test/src/testIntegration/java/com/liferay/portal/workflow/metrics/rest/resource/v1_0/test/helper/WorkflowMetricsRESTTestHelper.java
+++ modules/dxp/apps/portal-workflow/portal-workflow-metrics-rest-test/src/testIntegration/java/com/liferay/portal/workflow/metrics/rest/resource/v1_0/test/helper/WorkflowMetricsRESTTestHelper.java
@@ -987,15 +987,13 @@ public class WorkflowMetricsRESTTestHelper {
 		_deleteDocuments(
 			_slaTaskResultWorkflowMetricsIndexNameBuilder.getIndexName(
 				companyId),
-			"WorkflowMetricsSLATaskResultType", "companyId", companyId,
-			"processId", processId);
+			"companyId", companyId, "processId", processId);
 	}
 
 	public void deleteTasks(long companyId, long processId) throws Exception {
 		_deleteDocuments(
 			_taskWorkflowMetricsIndexNameBuilder.getIndexName(companyId),
-			"WorkflowMetricsTaskType", "companyId", companyId, "processId",
-			processId);
+			"companyId", companyId, "processId", processId);
 	}
 
 	public Document[] getDocuments(long companyId) throws Exception {
@@ -1278,8 +1276,7 @@ public class WorkflowMetricsRESTTestHelper {
 		return documentBuilder.build();
 	}
 
-	private void _deleteDocuments(
-			String indexName, String indexType, Object... parameters)
+	private void _deleteDocuments(String indexName, Object... parameters)
 		throws Exception {
 
 		if (_searchEngineAdapter == null) {
@@ -1301,7 +1298,6 @@ public class WorkflowMetricsRESTTestHelper {
 		searchSearchRequest.setQuery(booleanQuery);
 
 		searchSearchRequest.setSize(10000);
-		searchSearchRequest.setTypes(indexType);
 
 		SearchSearchResponse searchSearchResponse =
 			_searchEngineAdapter.execute(searchSearchRequest);
@@ -1331,7 +1327,6 @@ public class WorkflowMetricsRESTTestHelper {
 						indexName, document.getString("uid"), document);
 
 				updateDocumentRequest.setRefresh(true);
-				updateDocumentRequest.setType(indexType);
 				updateDocumentRequest.setUpsert(true);
 
 				_searchEngineAdapter.execute(updateDocumentRequest);
