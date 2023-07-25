d81a1e8e3b9cb74565f245cdd02466aaa057049b
Joshua Cords <joshua.cords@liferay.com>
2023-07-25 13:56:52 -0700

    LPS-191707 portal-search-spi: add setPermissionAware
    https://liferay.atlassian.net/browse/LPS-191707

--- modules/apps/portal-search/portal-search-spi/src/main/java/com/liferay/portal/search/spi/model/registrar/ModelSearchDefinition.java
+++ modules/apps/portal-search/portal-search-spi/src/main/java/com/liferay/portal/search/spi/model/registrar/ModelSearchDefinition.java
@@ -41,6 +41,8 @@ public interface ModelSearchDefinition {
 	public void setModelVisibilityContributor(
 		ModelVisibilityContributor modelVisibilityContributor);
 
+	public void setPermissionAware(boolean permissionAware);
+
 	public void setSearchResultPermissionFilterSuppressed(
 		boolean searchResultPermissionFilterSuppressed);
 
