0caef8b0d730d71865a83d543b973a069b24b6a8
Bryan Engler <bryan.engler@liferay.com>
2023-08-11 15:18:58 -0700

    LPS-194028 LPS-194037 portal-search-elasticsearch7-impl: Process contributed settings as well
    https://liferay.atlassian.net/browse/LPS-194037

--- modules/apps/portal-search-elasticsearch7/portal-search-elasticsearch7-impl/src/main/java/com/liferay/portal/search/elasticsearch7/internal/index/CompanyIndexFactoryHelper.java
+++ modules/apps/portal-search-elasticsearch7/portal-search-elasticsearch7-impl/src/main/java/com/liferay/portal/search/elasticsearch7/internal/index/CompanyIndexFactoryHelper.java
@@ -32,6 +32,7 @@ import java.io.IOException;
 import org.elasticsearch.action.ActionResponse;
 import org.elasticsearch.action.admin.indices.create.CreateIndexRequest;
 import org.elasticsearch.action.admin.indices.delete.DeleteIndexRequest;
+import org.elasticsearch.action.admin.indices.settings.put.UpdateSettingsRequest;
 import org.elasticsearch.client.IndicesClient;
 import org.elasticsearch.client.RequestOptions;
 import org.elasticsearch.client.RestHighLevelClient;
@@ -312,12 +313,6 @@ public class CompanyIndexFactoryHelper {
 	private void _processContributions(
 		IndexConfigurationContributor indexConfigurationContributor) {
 
-		if (Validator.isNotNull(
-				_elasticsearchConfigurationWrapper.overrideTypeMappings())) {
-
-			return;
-		}
-
 		RestHighLevelClient restHighLevelClient = null;
 
 		try {
@@ -330,6 +325,47 @@ public class CompanyIndexFactoryHelper {
 			if (_log.isInfoEnabled()) {
 				_log.info("Skipping index settings contributor");
 			}
+
+			return;
+		}
+
+		SettingsBuilder settingsBuilder = new SettingsBuilder(
+			Settings.builder());
+
+		indexConfigurationContributor.contributeSettings(settingsBuilder::put);
+
+		Settings settings = settingsBuilder.build();
+
+		if (!settings.isEmpty()) {
+			IndicesClient indicesClient = restHighLevelClient.indices();
+
+			for (Long companyId :
+					IndexFactoryCompanyIdRegistryUtil.getCompanyIds()) {
+
+				String indexName = getIndexName(companyId);
+
+				UpdateSettingsRequest updateSettingsRequest =
+					new UpdateSettingsRequest(indexName);
+
+				updateSettingsRequest.settings(settings);
+
+				try {
+					indicesClient.putSettings(
+						updateSettingsRequest, RequestOptions.DEFAULT);
+				}
+				catch (Exception exception) {
+					_log.error(
+						StringBundler.concat(
+							"Unable to put settings for index ", indexName,
+							" with contributor ",
+							indexConfigurationContributor),
+						exception);
+				}
+			}
+		}
+
+		if (Validator.isNotNull(
+				_elasticsearchConfigurationWrapper.overrideTypeMappings())) {
 
 			return;
 		}
