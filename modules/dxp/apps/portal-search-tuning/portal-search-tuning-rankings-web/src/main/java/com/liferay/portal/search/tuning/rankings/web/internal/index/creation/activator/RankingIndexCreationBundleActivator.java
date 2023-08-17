32eb71509c51d212a4c6c45a12d42eaf8b1bc226
Almir Ferreira <almir.ferreira@liferay.com>
2023-08-17 16:29:35 -0300

    LPS-191765 portal-search-tuning-rankings-web: Change RankingIndexCreationBundleActivator class to create index instead of adding backgroundtask
    https://liferay.atlassian.net/browse/LPS-191765

--- modules/dxp/apps/portal-search-tuning/portal-search-tuning-rankings-web/src/main/java/com/liferay/portal/search/tuning/rankings/web/internal/index/creation/activator/RankingIndexCreationBundleActivator.java
+++ modules/dxp/apps/portal-search-tuning/portal-search-tuning-rankings-web/src/main/java/com/liferay/portal/search/tuning/rankings/web/internal/index/creation/activator/RankingIndexCreationBundleActivator.java
@@ -5,20 +5,9 @@
 
 package com.liferay.portal.search.tuning.rankings.web.internal.index.creation.activator;
 
-import com.liferay.portal.kernel.backgroundtask.BackgroundTaskExecutor;
-import com.liferay.portal.kernel.backgroundtask.BackgroundTaskManager;
-import com.liferay.portal.kernel.exception.PortalException;
-import com.liferay.portal.kernel.log.Log;
-import com.liferay.portal.kernel.log.LogFactoryUtil;
-import com.liferay.portal.kernel.model.CompanyConstants;
-import com.liferay.portal.kernel.model.UserConstants;
-import com.liferay.portal.kernel.service.ServiceContext;
-import com.liferay.portal.kernel.uuid.PortalUUID;
 import com.liferay.portal.search.engine.SearchEngineInformation;
-import com.liferay.portal.search.tuning.rankings.web.internal.background.task.RankingIndexCreationBackgroundTaskExecutor;
 import com.liferay.portal.search.tuning.rankings.web.internal.index.importer.SingleIndexToMultipleIndexImporter;
 
-import java.util.HashMap;
 import java.util.Objects;
 
 import org.osgi.service.component.annotations.Activate;
@@ -41,38 +30,9 @@ public class RankingIndexCreationBundleActivator {
 		}
 
 		if (_singleIndexToMultipleIndexImporter.needImport()) {
-			_addBackgroundTask();
+			_singleIndexToMultipleIndexImporter.importRankings();
 		}
 	}
-
-	private void _addBackgroundTask() {
-		try {
-			_backgroundTaskManager.addBackgroundTask(
-				UserConstants.USER_ID_DEFAULT, CompanyConstants.SYSTEM,
-				"createRankingIndex-" + _portalUUID.generate(),
-				RankingIndexCreationBackgroundTaskExecutor.class.getName(),
-				new HashMap<>(), new ServiceContext());
-		}
-		catch (PortalException portalException) {
-			_log.error(
-				"Unable to schedule the job for RankingIndexRename",
-				portalException);
-		}
-	}
-
-	private static final Log _log = LogFactoryUtil.getLog(
-		RankingIndexCreationBundleActivator.class);
-
-	@Reference
-	private BackgroundTaskManager _backgroundTaskManager;
-
-	@Reference
-	private PortalUUID _portalUUID;
-
-	@Reference(
-		target = "(background.task.executor.class.name=com.liferay.portal.search.tuning.rankings.web.internal.background.task.RankingIndexCreationBackgroundTaskExecutor)"
-	)
-	private BackgroundTaskExecutor _rankingIndexRenameBackgroundTaskExecutor;
 
 	@Reference
 	private SearchEngineInformation _searchEngineInformation;
