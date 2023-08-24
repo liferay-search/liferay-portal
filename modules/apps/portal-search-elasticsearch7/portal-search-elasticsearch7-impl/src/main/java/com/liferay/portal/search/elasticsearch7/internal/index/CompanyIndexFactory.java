209dab613523d7e7ac744630918bc176d15ac72c
Bryan Engler <bryan.engler@liferay.com>
2023-08-24 08:57:41 -0700

    LPS-194028 LPS-194526 portal-search-elasticsearch7-impl: Sort
    https://liferay.atlassian.net/browse/LPS-194526

--- modules/apps/portal-search-elasticsearch7/portal-search-elasticsearch7-impl/src/main/java/com/liferay/portal/search/elasticsearch7/internal/index/CompanyIndexFactory.java
+++ modules/apps/portal-search-elasticsearch7/portal-search-elasticsearch7-impl/src/main/java/com/liferay/portal/search/elasticsearch7/internal/index/CompanyIndexFactory.java
@@ -5,7 +5,6 @@
 
 package com.liferay.portal.search.elasticsearch7.internal.index;
 
-import com.liferay.petra.string.StringBundler;
 import com.liferay.portal.kernel.log.Log;
 import com.liferay.portal.kernel.log.LogFactoryUtil;
 import com.liferay.portal.kernel.model.Company;
