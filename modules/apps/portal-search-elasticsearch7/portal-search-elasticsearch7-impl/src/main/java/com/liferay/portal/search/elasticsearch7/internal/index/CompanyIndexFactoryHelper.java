4cff497b6fdd804f108472ddb1786b4035fdc401
Bryan Engler <bryan.engler@liferay.com>
2023-08-25 07:19:09 -0700

    LPD-7822 LPD-8355 portal-search-elasticsearch7-impl: Rename methods and file
    https://liferay.atlassian.net/browse/LPD-8355

--- modules/apps/portal-search-elasticsearch7/portal-search-elasticsearch7-impl/src/main/java/com/liferay/portal/search/elasticsearch7/internal/index/CompanyIndexFactoryHelper.java
+++ modules/apps/portal-search-elasticsearch7/portal-search-elasticsearch7-impl/src/main/java/com/liferay/portal/search/elasticsearch7/internal/index/CompanyIndexFactoryHelper.java
@@ -184,7 +184,7 @@ public class CompanyIndexFactoryHelper {
 		}
 	}
 
-	protected void loadAdditionalTypeMappings(
+	private void _putAdditionalTypeMappings(
 		LiferayDocumentTypeFactory liferayDocumentTypeFactory) {
 
 		if (Validator.isNull(
@@ -311,7 +311,7 @@ public class CompanyIndexFactoryHelper {
 		settingsBuilder.put("index.translog.sync_interval", "100ms");
 	}
 
-	private void _loadTypeMappingsContributors(
+	private void _putContributedTypeMappings(
 		LiferayDocumentTypeFactory liferayDocumentTypeFactory) {
 
 		for (IndexConfigurationContributor indexConfigurationContributor :
@@ -398,11 +398,11 @@ public class CompanyIndexFactoryHelper {
 			return;
 		}
 
-		loadAdditionalTypeMappings(liferayDocumentTypeFactory);
+		_putAdditionalTypeMappings(liferayDocumentTypeFactory);
 
-		_loadTypeMappingsContributors(liferayDocumentTypeFactory);
+		_putContributedTypeMappings(liferayDocumentTypeFactory);
 
-		liferayDocumentTypeFactory.createOptionalDefaultTypeMappings();
+		liferayDocumentTypeFactory.putDefaultTypeMappingTemplate();
 	}
 
 	private static final Log _log = LogFactoryUtil.getLog(
