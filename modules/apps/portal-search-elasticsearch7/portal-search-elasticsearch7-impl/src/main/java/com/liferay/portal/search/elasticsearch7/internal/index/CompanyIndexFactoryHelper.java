fbf0b0d3576e0f23c63b4738446baaacd0bcde38
Bryan Engler <bryan.engler@liferay.com>
2023-08-25 07:12:41 -0700

    LPS-194028 LPS-194033 portal-search-elasticsearch7-impl: Apply
    https://liferay.atlassian.net/browse/LPS-194033

--- modules/apps/portal-search-elasticsearch7/portal-search-elasticsearch7-impl/src/main/java/com/liferay/portal/search/elasticsearch7/internal/index/CompanyIndexFactoryHelper.java
+++ modules/apps/portal-search-elasticsearch7/portal-search-elasticsearch7-impl/src/main/java/com/liferay/portal/search/elasticsearch7/internal/index/CompanyIndexFactoryHelper.java
@@ -293,7 +293,7 @@ public class CompanyIndexFactoryHelper {
 		for (IndexSettingsContributor indexSettingsContributor :
 				_indexSettingsContributorServiceTrackerList) {
 
-			indexSettingsContributor.populate(builder::put);
+			indexSettingsContributor.contributeSettings(builder::put);
 		}
 	}
 
