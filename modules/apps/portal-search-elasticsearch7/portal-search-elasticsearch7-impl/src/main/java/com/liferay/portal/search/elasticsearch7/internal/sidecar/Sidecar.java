0c285a1ba6bb5f7827f41d0079922e0bb584019a
Bryan Engler <bryan.engler@liferay.com>
2023-08-25 08:15:18 -0700

    LPS-194028 LPS-194039 portal-search-elasticsearch7-impl: Rename constants class
    https://liferay.atlassian.net/browse/LPS-194039

--- modules/apps/portal-search-elasticsearch7/portal-search-elasticsearch7-impl/src/main/java/com/liferay/portal/search/elasticsearch7/internal/sidecar/Sidecar.java
+++ modules/apps/portal-search-elasticsearch7/portal-search-elasticsearch7-impl/src/main/java/com/liferay/portal/search/elasticsearch7/internal/sidecar/Sidecar.java
@@ -22,7 +22,7 @@ import com.liferay.portal.kernel.util.ListUtil;
 import com.liferay.portal.kernel.util.StringUtil;
 import com.liferay.portal.kernel.util.Validator;
 import com.liferay.portal.search.elasticsearch7.internal.configuration.ElasticsearchConfigurationWrapper;
-import com.liferay.portal.search.elasticsearch7.internal.sidecar.constants.SidecarVersionConstants;
+import com.liferay.portal.search.elasticsearch7.internal.sidecar.constants.SidecarConstants;
 import com.liferay.portal.search.elasticsearch7.internal.util.ResourceUtil;
 
 import java.io.File;
@@ -283,7 +283,7 @@ public class Sidecar {
 
 	private Distribution _getElasticsearchDistribution() {
 		String versionNumber = ResourceUtil.getResourceAsString(
-			getClass(), SidecarVersionConstants.SIDECAR_VERSION_FILE_NAME);
+			getClass(), SidecarConstants.SIDECAR_VERSION_FILE_NAME);
 
 		if (versionNumber.equals(ElasticsearchDistribution.VERSION)) {
 			return new ElasticsearchDistribution();
