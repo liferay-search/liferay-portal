0bea85842413b75e0e7d042c3b46d9e81a53d439
Bryan Engler <bryan.engler@liferay.com>
2023-08-24 18:17:02 -0700

    LPD-7822 LPD-8358 portal-search-elasticsearch7-impl: Move loadDefaultAnalyzers into _loadDefaultIndexSettings
    https://liferay.atlassian.net/browse/LPD-8358

--- modules/apps/portal-search-elasticsearch7/portal-search-elasticsearch7-impl/src/main/java/com/liferay/portal/search/elasticsearch7/internal/index/CompanyIndexFactoryHelper.java
+++ modules/apps/portal-search-elasticsearch7/portal-search-elasticsearch7-impl/src/main/java/com/liferay/portal/search/elasticsearch7/internal/index/CompanyIndexFactoryHelper.java
@@ -236,7 +236,12 @@ public class CompanyIndexFactoryHelper {
 			_elasticsearchConfigurationWrapper.additionalIndexConfigurations());
 	}
 
-	private void _loadDefaultIndexSettings(SettingsBuilder settingsBuilder) {
+	private void _loadDefaultIndexSettings(
+		SettingsBuilder settingsBuilder,
+		LiferayDocumentTypeFactory liferayDocumentTypeFactory) {
+
+		liferayDocumentTypeFactory.loadDefaultAnalyzers(settingsBuilder);
+
 		String defaultIndexSettings = ResourceUtil.getResourceAsString(
 			getClass(), "/META-INF/settings/index-settings-defaults.json");
 
@@ -383,9 +388,7 @@ public class CompanyIndexFactoryHelper {
 		SettingsBuilder settingsBuilder = new SettingsBuilder(
 			Settings.builder());
 
-		liferayDocumentTypeFactory.loadDefaultAnalyzers(settingsBuilder);
-
-		_loadDefaultIndexSettings(settingsBuilder);
+		_loadDefaultIndexSettings(settingsBuilder, liferayDocumentTypeFactory);
 
 		_loadTestModeIndexSettings(settingsBuilder);
 
