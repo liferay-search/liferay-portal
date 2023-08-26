53788e87d5804774b81bfb7ad604b7e424f1d17c
Bryan Engler <bryan.engler@liferay.com>
2023-08-26 09:51:11 -0700

    LPD-7822 LPD-8355 portal-search-elasticsearch7-impl: Apply
    https://liferay.atlassian.net/browse/LPD-8355

--- modules/apps/portal-search-elasticsearch7/portal-search-elasticsearch7-impl/src/main/java/com/liferay/portal/search/elasticsearch7/internal/index/CompanyIndexFactoryHelper.java
+++ modules/apps/portal-search-elasticsearch7/portal-search-elasticsearch7-impl/src/main/java/com/liferay/portal/search/elasticsearch7/internal/index/CompanyIndexFactoryHelper.java
@@ -58,7 +58,8 @@ public class CompanyIndexFactoryHelper {
 			indexName);
 
 		LiferayDocumentTypeFactory liferayDocumentTypeFactory =
-			new LiferayDocumentTypeFactory(indicesClient, _jsonFactory);
+			new LiferayDocumentTypeFactory(
+				indexName, indicesClient, _jsonFactory);
 
 		_setSettings(createIndexRequest, liferayDocumentTypeFactory);
 
@@ -328,12 +329,13 @@ public class CompanyIndexFactoryHelper {
 			return;
 		}
 
-		LiferayDocumentTypeFactory liferayDocumentTypeFactory =
-			new LiferayDocumentTypeFactory(
-				restHighLevelClient.indices(), _jsonFactory);
-
 		for (Long companyId :
 				IndexFactoryCompanyIdRegistryUtil.getCompanyIds()) {
+
+			LiferayDocumentTypeFactory liferayDocumentTypeFactory =
+				new LiferayDocumentTypeFactory(
+					getIndexName(companyId), restHighLevelClient.indices(),
+					_jsonFactory);
 
 			indexConfigurationContributor.contributeMappings(
 				getIndexName(companyId), liferayDocumentTypeFactory);
