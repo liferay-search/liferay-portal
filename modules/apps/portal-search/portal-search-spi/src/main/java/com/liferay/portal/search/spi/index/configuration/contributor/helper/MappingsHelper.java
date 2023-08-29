b5fd97d9e96e3fcc47206062425995db5ffa9f72
Bryan Engler <bryan.engler@liferay.com>
2023-08-29 11:04:10 -0700

    LPD-7822 LPD-8363 portal-search-spi: Rename putTypeMappings to putMappings
    https://liferay.atlassian.net/browse/LPD-8363
    
    # breaking
    
    ## What modules/apps/portal-search/portal-search-spi/src/main/java/com/liferay/portal/search/spi/index/configuration/contributor/helper/MappingsHelper.java
    
    The method putTypeMappings() was renamed to putMappings()
    
    ## Why
    
    The word "type" is irrelevant and obsolete in regards to mappings
    
    ----

--- modules/apps/portal-search/portal-search-spi/src/main/java/com/liferay/portal/search/spi/index/configuration/contributor/helper/MappingsHelper.java
+++ modules/apps/portal-search/portal-search-spi/src/main/java/com/liferay/portal/search/spi/index/configuration/contributor/helper/MappingsHelper.java
@@ -10,6 +10,6 @@ package com.liferay.portal.search.spi.index.configuration.contributor.helper;
  */
 public interface MappingsHelper {
 
-	public void putTypeMappings(String source);
+	public void putMappings(String source);
 
 }
