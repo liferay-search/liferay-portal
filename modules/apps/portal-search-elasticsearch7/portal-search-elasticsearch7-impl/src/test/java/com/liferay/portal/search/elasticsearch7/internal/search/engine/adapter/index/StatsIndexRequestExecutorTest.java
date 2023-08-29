a005670597403ea1ede003ead98d2944632e327e
Felipe Lorenz <felipe.lorenz@liferay.com>
2023-08-29 14:27:08 -0300

    LPS-187293 LPS-194514 portal-search-elasticsearch7-impl: Add tests for StatsIndexRequestExecutorImpl
    https://liferay.atlassian.net/browse/LPS-194514

---
+++ modules/apps/portal-search-elasticsearch7/portal-search-elasticsearch7-impl/src/test/java/com/liferay/portal/search/elasticsearch7/internal/search/engine/adapter/index/StatsIndexRequestExecutorTest.java
@@ -0,0 +1,70 @@
+/**
+ * SPDX-FileCopyrightText: (c) 2023 Liferay, Inc. https://liferay.com
+ * SPDX-License-Identifier: LGPL-2.1-or-later OR LicenseRef-Liferay-DXP-EULA-2.0.0-2023-06
+ */
+
+package com.liferay.portal.search.elasticsearch7.internal.search.engine.adapter.index;
+
+import com.liferay.portal.json.JSONFactoryImpl;
+import com.liferay.portal.kernel.test.ReflectionTestUtil;
+import com.liferay.portal.search.elasticsearch7.internal.connection.ElasticsearchFixture;
+import com.liferay.portal.search.engine.adapter.index.StatsIndexRequest;
+import com.liferay.portal.search.engine.adapter.index.StatsIndexResponse;
+import com.liferay.portal.test.rule.LiferayUnitTestRule;
+
+import org.junit.After;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.ClassRule;
+import org.junit.Test;
+
+/**
+ * @author Felipe Lorenz
+ */
+public class StatsIndexRequestExecutorTest {
+
+	@ClassRule
+	public static LiferayUnitTestRule liferayUnitTestRule =
+		LiferayUnitTestRule.INSTANCE;
+
+	@Before
+	public void setUp() throws Exception {
+		_elasticsearchFixture = new ElasticsearchFixture(
+			StatsIndexRequestExecutorTest.class.getSimpleName());
+
+		_elasticsearchFixture.setUp();
+	}
+
+	@After
+	public void tearDown() throws Exception {
+		_elasticsearchFixture.tearDown();
+	}
+
+	@Test
+	public void testIndexRequestExecution() {
+		StatsIndexRequest statsIndexRequest = new StatsIndexRequest(
+			_INDEX_NAME);
+
+		StatsIndexRequestExecutorImpl statsIndexRequestExecutorImpl =
+			new StatsIndexRequestExecutorImpl();
+
+		ReflectionTestUtil.setFieldValue(
+			statsIndexRequestExecutorImpl, "_elasticsearchClientResolver",
+			_elasticsearchFixture);
+		ReflectionTestUtil.setFieldValue(
+			statsIndexRequestExecutorImpl, "_jsonFactory",
+			new JSONFactoryImpl());
+
+		StatsIndexResponse statsIndexResponse =
+			statsIndexRequestExecutorImpl.execute(statsIndexRequest);
+
+		Assert.assertNotNull(statsIndexResponse);
+
+		Assert.assertNotNull(statsIndexResponse.getTotalSpace());
+	}
+
+	private static final String _INDEX_NAME = "liferay";
+
+	private ElasticsearchFixture _elasticsearchFixture;
+
+}
