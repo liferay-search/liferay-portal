3f5eb1a5cc27e5c2a71c0554310434bf6680cb94
Bryan Engler <bryan.engler@liferay.com>
2023-08-30 08:33:33 -0700

    LPS-194028 LPS-194527 portal-search-elasticsearch7-impl: Sort
    https://liferay.atlassian.net/browse/LPS-194527

--- modules/apps/portal-search-elasticsearch7/portal-search-elasticsearch7-impl/src/main/java/com/liferay/portal/search/elasticsearch7/internal/index/CompanyIndexFactoryHelper.java
+++ modules/apps/portal-search-elasticsearch7/portal-search-elasticsearch7-impl/src/main/java/com/liferay/portal/search/elasticsearch7/internal/index/CompanyIndexFactoryHelper.java
@@ -250,18 +250,6 @@ public class CompanyIndexFactoryHelper {
 			_elasticsearchConfigurationWrapper.additionalIndexConfigurations());
 	}
 
-	private void _loadDefaultIndexSettings(
-		SettingsBuilder settingsBuilder,
-		LiferayDocumentTypeFactory liferayDocumentTypeFactory) {
-
-		liferayDocumentTypeFactory.loadDefaultAnalyzers(settingsBuilder);
-
-		String defaultIndexSettings = ResourceUtil.getResourceAsString(
-			getClass(), "/META-INF/settings/index-settings-defaults.json");
-
-		settingsBuilder.loadFromSource(defaultIndexSettings);
-	}
-
 	private void _loadConfigurationIndexSettings(
 		SettingsBuilder settingsBuilder) {
 
@@ -275,6 +263,18 @@ public class CompanyIndexFactoryHelper {
 			"index.max_result_window",
 			String.valueOf(
 				_elasticsearchConfigurationWrapper.indexMaxResultWindow()));
+	}
+
+	private void _loadDefaultIndexSettings(
+		SettingsBuilder settingsBuilder,
+		LiferayDocumentTypeFactory liferayDocumentTypeFactory) {
+
+		liferayDocumentTypeFactory.loadDefaultAnalyzers(settingsBuilder);
+
+		String defaultIndexSettings = ResourceUtil.getResourceAsString(
+			getClass(), "/META-INF/settings/index-settings-defaults.json");
+
+		settingsBuilder.loadFromSource(defaultIndexSettings);
 	}
 
 	private void _loadIndexConfigurationContributors(
@@ -310,17 +310,6 @@ public class CompanyIndexFactoryHelper {
 				settingsBuilder.get("index.number_of_replicas"))) {
 
 			settingsBuilder.put("index.auto_expand_replicas", false);
-		}
-	}
-
-	private void _putContributedTypeMappings(
-		LiferayDocumentTypeFactory liferayDocumentTypeFactory) {
-
-		for (IndexConfigurationContributor indexConfigurationContributor :
-				_indexConfigurationContributorServiceTrackerList) {
-
-			indexConfigurationContributor.contributeMappings(
-				liferayDocumentTypeFactory);
 		}
 	}
 
@@ -409,6 +398,17 @@ public class CompanyIndexFactoryHelper {
 
 		liferayDocumentTypeFactory.putTypeMappings(
 			_elasticsearchConfigurationWrapper.additionalTypeMappings());
+	}
+
+	private void _putContributedTypeMappings(
+		LiferayDocumentTypeFactory liferayDocumentTypeFactory) {
+
+		for (IndexConfigurationContributor indexConfigurationContributor :
+				_indexConfigurationContributorServiceTrackerList) {
+
+			indexConfigurationContributor.contributeMappings(
+				liferayDocumentTypeFactory);
+		}
 	}
 
 	private void _setMappings(
