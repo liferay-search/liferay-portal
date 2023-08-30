da30351a408bd93b66596e46980b1f392009d0fb
Bryan Engler <bryan.engler@liferay.com>
2023-08-30 08:51:23 -0700

    LPS-194028 LPS-194033 commerce-elasticsearch7: Apply
    https://liferay.atlassian.net/browse/LPS-194033

--- modules/apps/commerce/commerce-elasticsearch7/src/main/java/com/liferay/commerce/elasticsearch7/internal/index/configuration/contributor/CommerceIndexConfigurationContributor.java
+++ modules/apps/commerce/commerce-elasticsearch7/src/main/java/com/liferay/commerce/elasticsearch7/internal/index/configuration/contributor/CommerceIndexConfigurationContributor.java
@@ -1,23 +1,23 @@
 /**
- * SPDX-FileCopyrightText: (c) 2000 Liferay, Inc. https://liferay.com
+ * SPDX-FileCopyrightText: (c) 2023 Liferay, Inc. https://liferay.com
  * SPDX-License-Identifier: LGPL-2.1-or-later OR LicenseRef-Liferay-DXP-EULA-2.0.0-2023-06
  */
 
-package com.liferay.commerce.elasticsearch7.internal.index.settings.contributor;
+package com.liferay.commerce.elasticsearch7.internal.index.configuration.contributor;
 
 import com.liferay.portal.kernel.util.StringUtil;
-import com.liferay.portal.search.spi.settings.IndexSettingsContributor;
-import com.liferay.portal.search.spi.settings.IndexSettingsHelper;
-import com.liferay.portal.search.spi.settings.TypeMappingsHelper;
+import com.liferay.portal.search.spi.index.configuration.contributor.IndexConfigurationContributor;
+import com.liferay.portal.search.spi.index.configuration.contributor.helper.IndexSettingsHelper;
+import com.liferay.portal.search.spi.index.configuration.contributor.helper.TypeMappingsHelper;
 
 import org.osgi.service.component.annotations.Component;
 
 /**
  * @author Marco Leo
  */
-@Component(service = IndexSettingsContributor.class)
-public class CommerceIndexSettingsContributor
-	implements IndexSettingsContributor {
+@Component(service = IndexConfigurationContributor.class)
+public class CommerceIndexConfigurationContributor
+	implements IndexConfigurationContributor {
 
 	@Override
 	public void contributeMappings(
