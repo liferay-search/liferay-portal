e5fedce7993bb7519cff2ad0bd24932f36aa8467
Bryan Engler <bryan.engler@liferay.com>
2023-08-30 08:21:59 -0700

    LPS-194028 LPS-194527 portal-search-elasticsearch7-impl: Update all user defined settings instead of just contributors
    https://liferay.atlassian.net/browse/LPS-194527

--- modules/apps/portal-search-elasticsearch7/portal-search-elasticsearch7-impl/src/main/java/com/liferay/portal/search/elasticsearch7/internal/index/CompanyIndexFactory.java
+++ modules/apps/portal-search-elasticsearch7/portal-search-elasticsearch7-impl/src/main/java/com/liferay/portal/search/elasticsearch7/internal/index/CompanyIndexFactory.java
@@ -15,9 +15,6 @@ import com.liferay.portal.search.elasticsearch7.internal.configuration.Elasticse
 import com.liferay.portal.search.elasticsearch7.internal.configuration.ElasticsearchConfigurationWrapper;
 import com.liferay.portal.search.elasticsearch7.internal.connection.ElasticsearchConnectionManager;
 import com.liferay.portal.search.elasticsearch7.internal.index.util.IndexFactoryCompanyIdRegistryUtil;
-import com.liferay.portal.search.engine.adapter.SearchEngineAdapter;
-import com.liferay.portal.search.engine.adapter.index.UpdateIndexSettingsIndexRequest;
-import com.liferay.portal.search.index.IndexNameBuilder;
 
 import org.elasticsearch.client.IndicesClient;
 import org.elasticsearch.client.RestHighLevelClient;
@@ -88,8 +85,6 @@ public class CompanyIndexFactory
 	@Override
 	public void onElasticsearchConfigurationUpdate() {
 		_initializeCompanyIndexes();
-
-		_updateMaxResultWindow();
 	}
 
 	@Override
@@ -134,32 +129,6 @@ public class CompanyIndexFactory
 		}
 	}
 
-	private void _updateMaxResultWindow() {
-		int maxResultWindow =
-			_elasticsearchConfigurationWrapper.indexMaxResultWindow();
-
-		for (Long companyId :
-				IndexFactoryCompanyIdRegistryUtil.getCompanyIds()) {
-
-			String indexName = _indexNameBuilder.getIndexName(companyId);
-
-			UpdateIndexSettingsIndexRequest updateIndexSettingsIndexRequest =
-				new UpdateIndexSettingsIndexRequest(indexName);
-
-			updateIndexSettingsIndexRequest.setSettings(
-				"{\"index.max_result_window\": " + maxResultWindow + "}");
-
-			_searchEngineAdapter.execute(updateIndexSettingsIndexRequest);
-
-			if (_log.isInfoEnabled()) {
-				_log.info(
-					StringBundler.concat(
-						"Updated index.max_result_window to ", maxResultWindow,
-						" for index ", indexName));
-			}
-		}
-	}
-
 	private static final Log _log = LogFactoryUtil.getLog(
 		CompanyIndexFactory.class);
 
@@ -175,11 +144,5 @@ public class CompanyIndexFactory
 
 	@Reference
 	private ElasticsearchConnectionManager _elasticsearchConnectionManager;
-
-	@Reference
-	private IndexNameBuilder _indexNameBuilder;
-
-	@Reference
-	private SearchEngineAdapter _searchEngineAdapter;
 
 }
