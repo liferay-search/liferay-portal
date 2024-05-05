e9eb701667647f3914859b0793f5b3baaad4dd91
Bryan Engler <bryan.engler@liferay.com>
2024-05-05 07:47:16 -0700

    LPD-7822 LPD-8359 portal-search-elasticsearch7-impl: Rename IndexFactory methods
    https://liferay.atlassian.net/browse/LPD-8359

--- modules/apps/portal-search-elasticsearch7/portal-search-elasticsearch7-impl/src/main/java/com/liferay/portal/search/elasticsearch7/internal/index/CompanyIndexFactory.java
+++ modules/apps/portal-search-elasticsearch7/portal-search-elasticsearch7-impl/src/main/java/com/liferay/portal/search/elasticsearch7/internal/index/CompanyIndexFactory.java
@@ -44,7 +44,9 @@ public class CompanyIndexFactory
 	}
 
 	@Override
-	public boolean createIndices(IndicesClient indicesClient, long companyId) {
+	public boolean initializeIndex(
+		IndicesClient indicesClient, long companyId) {
+
 		String indexName = _companyIndexFactoryHelper.getIndexName(companyId);
 
 		if (_companyIndexFactoryHelper.hasIndex(indicesClient, indexName)) {
@@ -57,7 +59,7 @@ public class CompanyIndexFactory
 	}
 
 	@Override
-	public boolean deleteIndices(IndicesClient indicesClient, long companyId) {
+	public boolean deleteIndex(IndicesClient indicesClient, long companyId) {
 		String indexName = _companyIndexFactoryHelper.getIndexName(companyId);
 
 		Company company = _companyLocalService.fetchCompany(companyId);
@@ -85,7 +87,7 @@ public class CompanyIndexFactory
 
 	@Override
 	public void onElasticsearchConfigurationUpdate() {
-		_createCompanyIndexes();
+		_initializeCompanyIndexes();
 
 		_updateMaxResultWindow();
 	}
@@ -104,7 +106,7 @@ public class CompanyIndexFactory
 	protected void activate(BundleContext bundleContext) {
 		_elasticsearchConfigurationWrapper.register(this);
 
-		_createCompanyIndexes();
+		_initializeCompanyIndexes();
 	}
 
 	@Deactivate
@@ -112,7 +114,7 @@ public class CompanyIndexFactory
 		_elasticsearchConfigurationWrapper.unregister(this);
 	}
 
-	private synchronized void _createCompanyIndexes() {
+	private synchronized void _initializeCompanyIndexes() {
 		for (Long companyId :
 				IndexFactoryCompanyIdRegistryUtil.getCompanyIds()) {
 
@@ -120,7 +122,7 @@ public class CompanyIndexFactory
 				RestHighLevelClient restHighLevelClient =
 					_elasticsearchConnectionManager.getRestHighLevelClient();
 
-				createIndices(restHighLevelClient.indices(), companyId);
+				initializeIndex(restHighLevelClient.indices(), companyId);
 			}
 			catch (Exception exception) {
 				if (_log.isWarnEnabled()) {
