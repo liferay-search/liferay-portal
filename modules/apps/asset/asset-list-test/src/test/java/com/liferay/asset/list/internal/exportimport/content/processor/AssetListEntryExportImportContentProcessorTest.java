a441ecd39c5e2c3381cf6f66376414fa543f8ac3
gustavolimav <gustavo.lima@liferay.com>
2024-09-13 09:42:56 -0300

    LPD-35048 asset-list-test: Move test
    https://liferay.atlassian.net/browse/LPD-35048

--- modules/apps/asset/asset-list-test/src/test/java/com/liferay/asset/list/internal/exportimport/content/processor/AssetListEntryExportImportContentProcessorTest.java
+++ modules/apps/asset/asset-list-test/src/test/java/com/liferay/asset/list/internal/exportimport/content/processor/AssetListEntryExportImportContentProcessorTest.java
@@ -11,12 +11,10 @@ import com.liferay.portal.kernel.test.ReflectionTestUtil;
 import com.liferay.portal.kernel.test.util.RandomTestUtil;
 import com.liferay.portal.kernel.xml.Element;
 import com.liferay.portal.test.rule.LiferayUnitTestRule;
-
 import org.junit.Before;
 import org.junit.ClassRule;
 import org.junit.Rule;
 import org.junit.Test;
-
 import org.mockito.Mockito;
 
 /**
