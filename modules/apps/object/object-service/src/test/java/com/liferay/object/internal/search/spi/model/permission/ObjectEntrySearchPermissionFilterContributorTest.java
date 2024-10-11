ec65f532fa9cba49d18812f50e76bbb578f6a94d
gustavolimav <gustavo.lima@liferay.com>
2024-10-11 15:02:11 -0300

    LPD-38774 object-service: Add unit test
    https://liferay.atlassian.net/browse/LPD-38774

---
+++ modules/apps/object/object-service/src/test/java/com/liferay/object/internal/search/spi/model/permission/ObjectEntrySearchPermissionFilterContributorTest.java
@@ -0,0 +1,215 @@
+/**
+ * SPDX-FileCopyrightText: (c) 2024 Liferay, Inc. https://liferay.com
+ * SPDX-License-Identifier: LGPL-2.1-or-later OR LicenseRef-Liferay-DXP-EULA-2.0.0-2023-06
+ */
+
+package com.liferay.object.internal.search.spi.model.permission;
+
+import com.liferay.account.model.AccountEntry;
+import com.liferay.account.model.AccountEntryUserRel;
+import com.liferay.account.service.AccountEntryUserRelLocalService;
+import com.liferay.object.model.ObjectDefinition;
+import com.liferay.portal.kernel.exception.PortalException;
+import com.liferay.portal.kernel.search.filter.BooleanFilter;
+import com.liferay.portal.kernel.security.permission.PermissionChecker;
+import com.liferay.portal.kernel.service.OrganizationLocalService;
+import com.liferay.portal.kernel.test.ReflectionTestUtil;
+import com.liferay.portal.kernel.test.util.RandomTestUtil;
+import com.liferay.portal.search.configuration.SearchPermissionCheckerConfiguration;
+import com.liferay.portal.test.rule.LiferayUnitTestRule;
+
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
+import org.junit.Assert;
+import org.junit.ClassRule;
+import org.junit.Rule;
+import org.junit.Test;
+
+import org.mockito.Mockito;
+
+/**
+ * @author Gustavo Lima
+ */
+public class ObjectEntrySearchPermissionFilterContributorTest {
+
+	@ClassRule
+	@Rule
+	public static final LiferayUnitTestRule liferayUnitTestRule =
+		LiferayUnitTestRule.INSTANCE;
+
+	@Test
+	public void testContribute() throws PortalException {
+		long activeAccountEntryId = RandomTestUtil.randomLong();
+		long inactiveAccountEntryId = RandomTestUtil.randomLong();
+		long userId = RandomTestUtil.randomLong();
+
+		ObjectEntrySearchPermissionFilterContributor
+			objectEntrySearchPermissionFilterContributor =
+				_setUpObjectEntrySearchPermissionFilterContributor(
+					inactiveAccountEntryId, activeAccountEntryId, userId);
+
+		BooleanFilter booleanFilter = new BooleanFilter();
+
+		objectEntrySearchPermissionFilterContributor.contribute(
+			booleanFilter, RandomTestUtil.randomLong(), new long[0], userId,
+			_getPermissionChecker(userId), ObjectDefinition.class.getName());
+
+		String booleanFilterString = booleanFilter.toString();
+
+		Assert.assertFalse(
+			booleanFilterString,
+			booleanFilterString.contains(
+				"accountEntryRestrictedObjectFieldValue=[" +
+					inactiveAccountEntryId + "]"));
+		Assert.assertTrue(
+			booleanFilterString,
+			booleanFilterString.contains(
+				"accountEntryRestrictedObjectFieldValue=[" +
+					activeAccountEntryId + "]"));
+	}
+
+	private AccountEntryUserRel _createAccountEntryRel(
+			long accountEntryId, boolean inactive)
+		throws PortalException {
+
+		AccountEntryUserRel accountEntryUserRel = Mockito.mock(
+			AccountEntryUserRel.class);
+
+		AccountEntry accountEntry = Mockito.mock(AccountEntry.class);
+
+		Mockito.when(
+			accountEntryUserRel.getAccountEntry()
+		).thenReturn(
+			accountEntry
+		);
+
+		Mockito.when(
+			accountEntry.getAccountEntryId()
+		).thenReturn(
+			accountEntryId
+		);
+
+		Mockito.when(
+			accountEntry.isInactive()
+		).thenReturn(
+			inactive
+		);
+
+		return accountEntryUserRel;
+	}
+
+	private PermissionChecker _getPermissionChecker(long userId) {
+		PermissionChecker permissionChecker = Mockito.mock(
+			PermissionChecker.class);
+
+		Mockito.when(
+			permissionChecker.getUserId()
+		).thenReturn(
+			userId
+		);
+
+		return permissionChecker;
+	}
+
+	private void _setUpAccountEntryRels(
+			long activeAccountEntryId, long inactiveAccountEntryId,
+			ObjectEntrySearchPermissionFilterContributor
+				objectEntrySearchPermissionFilterContributor,
+			long userId)
+		throws PortalException {
+
+		AccountEntryUserRelLocalService accountEntryUserRelLocalService =
+			_setUpAccountEntryUserRelLocalService(
+				objectEntrySearchPermissionFilterContributor);
+
+		List<AccountEntryUserRel> accountEntryRelList = Arrays.asList(
+			_createAccountEntryRel(inactiveAccountEntryId, true),
+			_createAccountEntryRel(activeAccountEntryId, false));
+
+		Mockito.when(
+			accountEntryUserRelLocalService.
+				getAccountEntryUserRelsByAccountUserId(userId)
+		).thenReturn(
+			accountEntryRelList
+		);
+	}
+
+	private AccountEntryUserRelLocalService
+		_setUpAccountEntryUserRelLocalService(
+			ObjectEntrySearchPermissionFilterContributor
+				objectEntrySearchPermissionFilterContributor) {
+
+		AccountEntryUserRelLocalService accountEntryUserRelLocalService =
+			Mockito.mock(AccountEntryUserRelLocalService.class);
+
+		ReflectionTestUtil.setFieldValue(
+			objectEntrySearchPermissionFilterContributor,
+			"_accountEntryUserRelLocalService",
+			accountEntryUserRelLocalService);
+
+		return accountEntryUserRelLocalService;
+	}
+
+	private ObjectEntrySearchPermissionFilterContributor
+			_setUpObjectEntrySearchPermissionFilterContributor(
+				long inactiveAccountEntryId, long activeAccountEntryId,
+				long userId)
+		throws PortalException {
+
+		ObjectEntrySearchPermissionFilterContributor
+			objectEntrySearchPermissionFilterContributor =
+				new ObjectEntrySearchPermissionFilterContributor();
+
+		_setUpObjectEntrySearchPermissionFilterContributor(
+			objectEntrySearchPermissionFilterContributor);
+		_setUpOrganizationLocalService(
+			objectEntrySearchPermissionFilterContributor, userId);
+		_setUpAccountEntryRels(
+			activeAccountEntryId, inactiveAccountEntryId,
+			objectEntrySearchPermissionFilterContributor, userId);
+
+		return objectEntrySearchPermissionFilterContributor;
+	}
+
+	private void _setUpObjectEntrySearchPermissionFilterContributor(
+		ObjectEntrySearchPermissionFilterContributor
+			objectEntrySearchPermissionFilterContributor) {
+
+		SearchPermissionCheckerConfiguration
+			searchPermissionCheckerConfiguration = Mockito.mock(
+				SearchPermissionCheckerConfiguration.class);
+
+		ReflectionTestUtil.setFieldValue(
+			objectEntrySearchPermissionFilterContributor,
+			"_searchPermissionCheckerConfiguration",
+			searchPermissionCheckerConfiguration);
+
+		Mockito.when(
+			searchPermissionCheckerConfiguration.permissionTermsLimit()
+		).thenReturn(
+			2
+		);
+	}
+
+	private void _setUpOrganizationLocalService(
+		ObjectEntrySearchPermissionFilterContributor
+			objectEntrySearchPermissionFilterContributor,
+		long userId) {
+
+		OrganizationLocalService organizationLocalService = Mockito.mock(
+			OrganizationLocalService.class);
+
+		ReflectionTestUtil.setFieldValue(
+			objectEntrySearchPermissionFilterContributor,
+			"_organizationLocalService", organizationLocalService);
+
+		Mockito.when(
+			organizationLocalService.getUserOrganizations(userId)
+		).thenReturn(
+			Collections.emptyList()
+		);
+	}
+
+}
