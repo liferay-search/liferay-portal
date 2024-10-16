c5bb9e4e46d91caf7dab5dc14e7c691a2744dcaa
Olivia Yu <olivia.yu@liferay.com>
2024-10-15 18:49:47 -0700

    LPD-39359 portal-search-web: Convert value to number for validation

--- modules/apps/portal-search/portal-search-web/src/main/resources/META-INF/resources/js/custom_range_facet.js
+++ modules/apps/portal-search/portal-search-web/src/main/resources/META-INF/resources/js/custom_range_facet.js
@@ -192,8 +192,8 @@ AUI.add(
 					{
 						[rangeRuleName]() {
 							return (
-								instance.fromInputPicker.val() <=
-								instance.toInputPicker.val()
+								Number(instance.fromInputPicker.val()) <=
+								Number(instance.toInputPicker.val())
 							);
 						},
 					},
