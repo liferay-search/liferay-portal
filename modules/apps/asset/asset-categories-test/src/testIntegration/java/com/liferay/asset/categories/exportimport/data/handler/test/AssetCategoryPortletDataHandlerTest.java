baad8e95c9c891a4b8cf56bff5974c6179de5057
Rodrigo Guedes de Souza <rodrigo.guedes@liferay.com>
2025-09-03 16:48:41 -0300

    LPD-56346 asset-categories-test: create test https://liferay.atlassian.net/browse/LPD-56346

--- modules/apps/asset/asset-categories-test/src/testIntegration/java/com/liferay/asset/categories/exportimport/data/handler/test/AssetCategoryPortletDataHandlerTest.java
+++ modules/apps/asset/asset-categories-test/src/testIntegration/java/com/liferay/asset/categories/exportimport/data/handler/test/AssetCategoryPortletDataHandlerTest.java
@@ -7,13 +7,37 @@ package com.liferay.asset.categories.exportimport.data.handler.test;
 
 import com.liferay.arquillian.extension.junit.bridge.junit.Arquillian;
 import com.liferay.asset.categories.admin.web.constants.AssetCategoriesAdminPortletKeys;
+import com.liferay.asset.kernel.model.AssetCategory;
+import com.liferay.asset.kernel.model.AssetVocabulary;
+import com.liferay.asset.kernel.service.AssetCategoryLocalService;
+import com.liferay.asset.kernel.service.AssetVocabularyLocalService;
+import com.liferay.exportimport.kernel.configuration.ExportImportConfigurationSettingsMapFactoryUtil;
+import com.liferay.exportimport.kernel.configuration.constants.ExportImportConfigurationConstants;
 import com.liferay.exportimport.kernel.lar.DataLevel;
+import com.liferay.exportimport.kernel.lar.PortletDataHandlerKeys;
+import com.liferay.exportimport.kernel.model.ExportImportConfiguration;
+import com.liferay.exportimport.kernel.service.ExportImportConfigurationLocalService;
+import com.liferay.exportimport.kernel.service.ExportImportLocalService;
 import com.liferay.exportimport.test.util.lar.BasePortletDataHandlerTestCase;
+import com.liferay.portal.kernel.model.CompanyConstants;
 import com.liferay.portal.kernel.test.rule.AggregateTestRule;
+import com.liferay.portal.kernel.test.util.FeatureFlagTestUtil;
+import com.liferay.portal.kernel.test.util.RandomTestUtil;
+import com.liferay.portal.kernel.test.util.ServiceContextTestUtil;
+import com.liferay.portal.kernel.test.util.TestPropsValues;
+import com.liferay.portal.kernel.util.HashMapBuilder;
+import com.liferay.portal.test.rule.FeatureFlag;
+import com.liferay.portal.test.rule.FeatureFlags;
+import com.liferay.portal.test.rule.Inject;
 import com.liferay.portal.test.rule.LiferayIntegrationTestRule;
+import com.liferay.portal.test.rule.PermissionCheckerMethodTestRule;
 
+import java.io.File;
+
+import org.junit.Assert;
 import org.junit.ClassRule;
 import org.junit.Rule;
+import org.junit.Test;
 import org.junit.runner.RunWith;
 
 /**
@@ -26,7 +50,88 @@ public class AssetCategoryPortletDataHandlerTest
 	@ClassRule
 	@Rule
 	public static final AggregateTestRule aggregateTestRule =
-		new LiferayIntegrationTestRule();
+		new AggregateTestRule(
+			new LiferayIntegrationTestRule(),
+			PermissionCheckerMethodTestRule.INSTANCE);
+
+	@FeatureFlags(
+		featureFlags = {
+			@FeatureFlag(value = "LPD-17564"), @FeatureFlag(value = "LPD-35914")
+		}
+	)
+	@Test
+	public void testExportImportAssetCategory() throws Exception {
+		try {
+			FeatureFlagTestUtil.invokeFeatureFlagListeners(
+				CompanyConstants.SYSTEM, true, "LPD-35914");
+
+			AssetVocabulary assetVocabulary =
+				_assetVocabularyLocalService.addVocabulary(
+					TestPropsValues.getUserId(), stagingGroup.getGroupId(),
+					RandomTestUtil.randomString(),
+					ServiceContextTestUtil.getServiceContext());
+
+			AssetCategory assetCategory =
+				_assetCategoryLocalService.addCategory(
+					TestPropsValues.getUserId(), stagingGroup.getGroupId(),
+					RandomTestUtil.randomString(),
+					assetVocabulary.getVocabularyId(),
+					ServiceContextTestUtil.getServiceContext());
+
+			File larFile = _exportImportLocalService.exportLayoutsAsFile(
+				_exportImportConfigurationLocalService.
+					addDraftExportImportConfiguration(
+						TestPropsValues.getUserId(),
+						ExportImportConfigurationConstants.TYPE_EXPORT_LAYOUT,
+						ExportImportConfigurationSettingsMapFactoryUtil.
+							buildExportLayoutSettingsMap(
+								TestPropsValues.getUser(),
+								stagingGroup.getGroupId(), false, new long[0],
+								HashMapBuilder.put(
+									PortletDataHandlerKeys.PORTLET_DATA,
+									new String[] {Boolean.TRUE.toString()}
+								).put(
+									PortletDataHandlerKeys.PORTLET_DATA + "_" +
+										AssetCategoriesAdminPortletKeys.
+											ASSET_CATEGORIES_ADMIN,
+									new String[] {Boolean.TRUE.toString()}
+								).build())));
+
+			_assetCategoryLocalService.deleteCategory(assetCategory);
+
+			ExportImportConfiguration exportImportConfiguration =
+				_exportImportConfigurationLocalService.
+					addDraftExportImportConfiguration(
+						TestPropsValues.getUserId(),
+						ExportImportConfigurationConstants.TYPE_IMPORT_LAYOUT,
+						ExportImportConfigurationSettingsMapFactoryUtil.
+							buildImportLayoutSettingsMap(
+								TestPropsValues.getUser(),
+								stagingGroup.getGroupId(), false, new long[0],
+								HashMapBuilder.put(
+									PortletDataHandlerKeys.PORTLET_DATA,
+									new String[] {Boolean.TRUE.toString()}
+								).put(
+									PortletDataHandlerKeys.PORTLET_DATA + "_" +
+										AssetCategoriesAdminPortletKeys.
+											ASSET_CATEGORIES_ADMIN,
+									new String[] {Boolean.TRUE.toString()}
+								).build()));
+
+			_exportImportLocalService.importLayouts(
+				exportImportConfiguration, larFile);
+
+			Assert.assertNotNull(
+				_assetCategoryLocalService.
+					fetchAssetCategoryByExternalReferenceCode(
+						assetCategory.getExternalReferenceCode(),
+						stagingGroup.getGroupId()));
+		}
+		finally {
+			FeatureFlagTestUtil.invokeFeatureFlagListeners(
+				CompanyConstants.SYSTEM, false, "LPD-35914");
+		}
+	}
 
 	@Override
 	protected void addStagedModels() throws Exception {
@@ -56,5 +161,18 @@ public class AssetCategoryPortletDataHandlerTest
 	protected boolean isDataSiteLevel() {
 		return true;
 	}
+
+	@Inject
+	private AssetCategoryLocalService _assetCategoryLocalService;
+
+	@Inject
+	private AssetVocabularyLocalService _assetVocabularyLocalService;
+
+	@Inject
+	private ExportImportConfigurationLocalService
+		_exportImportConfigurationLocalService;
+
+	@Inject
+	private ExportImportLocalService _exportImportLocalService;
 
 }
