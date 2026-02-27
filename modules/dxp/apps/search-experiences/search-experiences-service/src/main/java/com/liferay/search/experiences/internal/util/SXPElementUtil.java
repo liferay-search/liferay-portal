044286a4f5f6cfbe5b1966c8255a616fe7141c53
Tibor Lipusz <tibor.lipusz@liferay.com>
2026-02-27 10:56:19 +0100

    LPD-80436 search-experiences-service: Add the Rescore element only in test mode
    Ongoing discussions about the future of Blueprints and elements necessitate a conservative approach; we should not add this element by default to new instances. It remains in the documentation so it can be added manually as before.
    
    We preserve it for testing. This also prevents a potential DuplicateSXPElementExternalReferenceCodeException in specific future cases. Refer to the ticket for more details.
    
    https://liferay.atlassian.net/browse/LPD-80436
    
    Signed-off-by: Tibor Lipusz <tibor.lipusz@liferay.com>

--- modules/dxp/apps/search-experiences/search-experiences-service/src/main/java/com/liferay/search/experiences/internal/util/SXPElementUtil.java
+++ modules/dxp/apps/search-experiences/search-experiences-service/src/main/java/com/liferay/search/experiences/internal/util/SXPElementUtil.java
@@ -11,6 +11,7 @@ import com.liferay.portal.kernel.model.Company;
 import com.liferay.portal.kernel.model.User;
 import com.liferay.portal.kernel.service.ServiceContext;
 import com.liferay.portal.kernel.util.LocaleUtil;
+import com.liferay.portal.kernel.util.PortalRunMode;
 import com.liferay.portal.kernel.util.StringUtil;
 import com.liferay.portal.kernel.util.URLUtil;
 import com.liferay.portal.vulcan.util.LocalizedMapUtil;
@@ -26,6 +27,7 @@ import java.util.ArrayList;
 import java.util.Enumeration;
 import java.util.HashSet;
 import java.util.List;
+import java.util.Objects;
 import java.util.Set;
 
 import org.osgi.framework.Bundle;
@@ -50,7 +52,11 @@ public class SXPElementUtil {
 		}
 
 		for (SXPElement sxpElement : _getOrCreateSXPElements()) {
-			if (externalReferenceCodes.contains(
+			if ((!PortalRunMode.isTestMode() &&
+				 Objects.equals(
+					 sxpElement.getExternalReferenceCode(),
+					 "RESCORE_BY_TEXT_EMBEDDING")) ||
+				externalReferenceCodes.contains(
 					sxpElement.getExternalReferenceCode())) {
 
 				continue;
