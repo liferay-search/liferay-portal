27c1db5df2a1a7e730070914ff0a5280f7b5062e
Bryan Engler <bryan.engler@liferay.com>
2026-04-02 12:17:20 -0700

    LPD-75970 Refactor current IndexReindexers a little bit
    https://liferay.atlassian.net/browse/LPD-75970

--- modules/apps/portal-search/portal-search-spi/src/main/java/com/liferay/portal/search/spi/reindexer/IndexReindexer.java
+++ modules/apps/portal-search/portal-search-spi/src/main/java/com/liferay/portal/search/spi/reindexer/IndexReindexer.java
@@ -10,8 +10,34 @@ package com.liferay.portal.search.spi.reindexer;
  */
 public interface IndexReindexer {
 
-	public void reindex(long companyId) throws Exception;
+	/**
+	 * @deprecated As of Cavanaugh (7.4.x), portal-search-spi, replaced by {@link
+	 *             #reindex(long, ExecutionMode)}
+	 */
+	@Deprecated
+	public default void reindex(long companyId) throws Exception {
+		reindex(companyId, ExecutionMode.FULL);
+	}
 
-	public void reindex(long companyId, String executionMode) throws Exception;
+	public void reindex(long companyId, ExecutionMode executionMode)
+		throws Exception;
+
+	public default void reindex(long companyId, String executionMode)
+		throws Exception {
+
+		reindex(companyId, ExecutionMode.valueOf(executionMode.toUpperCase()));
+	}
+
+	/*
+	create enum instead of String so that developers know what the
+	different modes are. this could maybe be in portal-search-api instead, and
+	also used for company index reindexing, but thats all internal, so probably
+	not really necessary
+	 */
+	public enum ExecutionMode {
+
+		CONCURRENT, FULL, SYNC
+
+	}
 
 }
