76014a8d93e1f6ed2f45c67ff235b3ff27230ba0
Rodrigo Guedes de Souza <rodrigo.guedes@liferay.com>
2026-05-21 10:20:59 -0300

    LPD-90488 portal-search-rest-api Add SemanticSearchConstants with MINIMUM_ELASTICSEARCH_VERSION https://liferay.atlassian.net/browse/LPD-90488

---
+++ modules/apps/portal-search/portal-search-rest-api/src/main/java/com/liferay/portal/search/rest/semantic/constants/SemanticSearchConstants.java
@@ -0,0 +1,15 @@
+/**
+ * SPDX-FileCopyrightText: (c) 2026 Liferay, Inc. https://liferay.com
+ * SPDX-License-Identifier: LGPL-2.1-or-later OR LicenseRef-Liferay-DXP-EULA-2.0.0-2023-06
+ */
+
+package com.liferay.portal.search.rest.semantic.constants;
+
+/**
+ * @author Rodrigo Guedes de Souza
+ */
+public class SemanticSearchConstants {
+
+	public static final String MINIMUM_ELASTICSEARCH_VERSION = "8.18.0";
+
+}
