aee90625d2cc5a8376fc78d77b088fb33e1e477f
Selena Aungst <selena.aungst@liferay.com>
2026-05-22 16:59:31 -0700

    LPD-90510 portal-search-opensearch2-impl: Add error message for rejected bulk items
    https://liferay.atlassian.net/browse/LPD-90510

--- modules/apps/portal-search-opensearch2/portal-search-opensearch2-impl/src/main/java/com/liferay/portal/search/opensearch2/internal/search/engine/adapter/document/BulkDocumentRequestExecutor.java
+++ modules/apps/portal-search-opensearch2/portal-search-opensearch2-impl/src/main/java/com/liferay/portal/search/opensearch2/internal/search/engine/adapter/document/BulkDocumentRequestExecutor.java
@@ -58,6 +58,9 @@ public class BulkDocumentRequestExecutor {
 		BulkDocumentResponse bulkDocumentResponse = new BulkDocumentResponse(
 			bulkResponse.took());
 
+		int rejectedItemsCount = 0;
+		String rejectionReason = null;
+
 		for (BulkResponseItem bulkResponseItem : bulkResponse.items()) {
 			BulkDocumentItemResponse bulkDocumentItemResponse =
 				new BulkDocumentItemResponse();
@@ -89,10 +92,30 @@ public class BulkDocumentRequestExecutor {
 				}
 
 				bulkDocumentResponse.setErrors(true);
+
+				if (bulkResponseItem.status() ==
+						_HTTP_STATUS_TOO_MANY_REQUESTS) {
+
+					rejectedItemsCount++;
+
+					if (rejectionReason == null) {
+						rejectionReason = errorCause.reason();
+					}
+				}
 			}
 
 			bulkDocumentResponse.addBulkDocumentItemResponse(
 				bulkDocumentItemResponse);
+		}
+
+		if (rejectedItemsCount > 0) {
+			throw new RuntimeException(
+				StringBundler.concat(
+					"Unable to index ", rejectedItemsCount, " of ",
+					bulkResponse.items(
+					).size(),
+					" bulk operations rejected by the search engine: ",
+					rejectionReason));
 		}
 
 		return bulkDocumentResponse;
@@ -184,6 +207,8 @@ public class BulkDocumentRequestExecutor {
 			}
 		}
 	}
+
+	private static final int _HTTP_STATUS_TOO_MANY_REQUESTS = 429;
 
 	private static final Log _log = LogFactoryUtil.getLog(
 		BulkDocumentRequestExecutor.class);
