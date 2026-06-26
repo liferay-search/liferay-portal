5876215e88c82e8ec5b609bcdc34a436a300cb61
Olivia Yu <olivia.yu@liferay.com>
2026-06-26 11:48:10 -0700

    LPD-78170 LPD-89039 asset-list-service: Include getSortSubfield so the order-by path maps text fields to the sortable value_keyword_lowercase

--- modules/apps/asset/asset-list-service/src/main/java/com/liferay/asset/list/internal/asset/entry/provider/AssetListAssetEntryProviderImpl.java
+++ modules/apps/asset/asset-list-service/src/main/java/com/liferay/asset/list/internal/asset/entry/provider/AssetListAssetEntryProviderImpl.java
@@ -984,7 +984,7 @@ public class AssetListAssetEntryProviderImpl
 
 		if (FeatureFlagManagerUtil.isEnabled(companyId, "LPD-74731")) {
 			orderByColumn = AssetListOrderByColumnsUtil.toOrderByColumn(
-				companyId, orderByColumn, LocaleUtil.getMostRelevantLocale());
+				companyId, orderByColumn);
 		}
 
 		if (orderByColumn.startsWith(StringPool.OPEN_CURLY_BRACE)) {
